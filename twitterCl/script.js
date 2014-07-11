52961e1363caabbdfe64a2f404e96ea13f0b54de
unknown <bernice.zerafa@MTDKP113.ix.com>
2014-07-11 14:40:53 +0200

    post tweet, refresh div on populate messages, methods for look up by hashtags / mention

--- twitterCl/script.js
+++ twitterCl/script.js
@@ -1,10 +1,17 @@
 function parseTags(content){
-	content.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
-	content.replace(/(^|\s)(@[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
+	//content.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
+	//content.replace(/(^|\s)(@[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
+	
+	content.replace(/#(\S+)/g, '<a href=\"list.html\">#$1</a>');
+	content.replace(/@(\S+)/g, '<a href=\"list.html\">@$1</a>');
+		
 	return content;
 }
 
 function documentReady() {
+	
+	populateAll();
+
 	$("#tweetBtn").click(function() {
 	
 		$.ajax({
@@ -15,14 +22,44 @@ function documentReady() {
 				content : $("#inputArea").val()
 			},
 			success: function(data) {
-				alert("added");
+				populateAll();
 			}
 		});
 	});
-	populateAll();
 }
 
+$(".hashtags").click(function() {
+	
+	var hashtag = $(this).val();
+	hashtag = hashtag.substring(1, hashtag.length);
+	
+	$.ajax({
+		type: "GET",
+		url:"http://localhost:8080/twitterlite/messages/hashtags/" + hashtag,
+		success: function(data) {
+			populateAll();
+		}
+	});
+});
+
+$(".mention").click(function() {
+	
+	var username = $(this).val();
+	username = username.substring(1, username.length);
+	
+	$.ajax({
+		type: "GET",
+		url:"http://localhost:8080/twitterlite/messages/mention/" + hashtag,
+		success: function(data) {
+			populateAll();
+		}
+	});
+});
+
 function populateAll(){
+
+	$("#allTweets").html("");
+
 	var request = $.ajax({
 		type: 'GET',
 		url:"http://localhost:8080/twitterlite/messages",
@@ -37,7 +74,7 @@ function populateAll(){
 		}).done(function(){
 			var data = $.parseJSON(request.responseText);
 			for (var i in data){
-				$("#allTweets").append("<div>"+parseTags(data[i].content)+"</div>");
+				$("#allTweets").prepend("<div>"+parseTags(data[i].content)+"</div>");
 			}
 		});
 
