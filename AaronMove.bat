98a85811ce5396a9efc703a7cb44dac032857cc6
unknown <bernice.zerafa@MTDKP113.ix.com>
2014-07-11 10:22:29 +0200

    batch file path

--- AaronMove.bat
+++ AaronMove.bat
@@ -1,1 +1,2 @@
 xcopy "C:\dev\repositories\git\twitterlite\twitterCl" "C:\dev\applications\tomcat\apache-tomcat-7.0.54\webapps\twitterCl" /D /E /C /R /I /K /Y
+xcopy "C:\dev\repositories\twitterlite\twitterCl" "C:\dev\applications\tomcat\apache-tomcat-7.0.54\webapps\twitterCl" /D /E /C /R /I /K /Y
