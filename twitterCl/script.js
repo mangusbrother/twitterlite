ffb70fdb9523ecb691daea0083d18700b9f83fe7
Administrator <admin@local.host>
2014-07-11 12:01:30 +0200

    made tweets appear under the new tweet area

--- twitterCl/script.js
+++ twitterCl/script.js
@@ -1,25 +1,28 @@
-function parseTags(elementId){
-	var content = $(elementId).html();
-	content = content.replace(/#(\\w+|\\W+)/g,function(text,link){
-		return '<a href="list.html?value='+link+'">'+link+'</a>';
-	})
-	content = content.replace(/@(\\w+|\\W+)/g,function(text,link){
-		return '<a href="list.html?value='+link+'">'+link+'</a>';
-	})
-	$(elementId).html(content);
-	
+function parseTags(content){
+	content.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
+	content.replace(/(^|\s)(@[a-z\d-]+)/ig, "$1<a href=\"list.html?value=$2\">$2</a>");
+	return content;
 }
 function tweet(){
 	alert("Test");
 }
 function populateAll(){
-	$.ajax({
+	var request = $.ajax({
 		type: 'GET',
 		url:"http://localhost:8080/twitterlite/messages",
-		data:{ get_param: 'value' },
-		success: function(data){
-			var names = data
-			$('allTweets').text(data);
-		}
-	);
+		/*
+			data is of the following format:
+				username: ""
+				content: ""
+				date: (ms)
+				hashtags: []
+				mentions: []
+		*/
+		}).done(function(){
+			var data = $.parseJSON(request.responseText);
+			for (var i in data){
+				$("#allTweets").append("<div>"+parseTags(data[i].content)+"</div>");
+			}
+		});
+
 }
