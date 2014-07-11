c0a49bd93137f110160540128a98dfdb94d750a6
unknown <bernice.zerafa@MTDKP113.ix.com>
2014-07-11 12:09:06 +0200

    post tweet

--- twitterCl/script.js
+++ twitterCl/script.js
@@ -1,25 +1,40 @@
-function parseTags(elementId){
-	var content = $(elementId).html();
-	content = content.replace(/#(\\w+|\\W+)/g,function(text,link){
-		return '<a href="list.html?value='+link+'">'+link+'</a>';
-	})
-	content = content.replace(/@(\\w+|\\W+)/g,function(text,link){
-		return '<a href="list.html?value='+link+'">'+link+'</a>';
-	})
-	$(elementId).html(content);
+function documentReady() {
 	
-}
-function tweet(){
-	alert("Test");
-}
-function populateAll(){
-	$.ajax({
-		type: 'GET',
-		url:"http://localhost:8080/twitterlite/messages",
-		data:{ get_param: 'value' },
-		success: function(data){
-			var names = data
-			$('allTweets').text(data);
-		}
-	);
-}
+	function parseTags(elementId){
+		var content = $(elementId).html();
+		content = content.replace(/#(\\w+|\\W+)/g,function(text,link){
+			return '<a href="list.html?value='+link+'">'+link+'</a>';
+		})
+		content = content.replace(/@(\\w+|\\W+)/g,function(text,link){
+			return '<a href="list.html?value='+link+'">'+link+'</a>';
+		})
+		$(elementId).html(content);
+	}
+
+	$("#tweetBtn").click(function() {
+	
+		$.ajax({
+			type: "POST",
+			url:"http://localhost:8080/twitterlite/tweets",
+			data: { 
+				username : $("#username").val(), 
+				content : $("#inputArea").val()
+			},
+			success: function(data) {
+				alert("added");
+			}
+		});
+	});
+
+	function populateAll(){
+		$.ajax({
+			type: 'GET',
+			url:"http://localhost:8080/twitterlite/messages",
+			data:{ get_param: 'value' },
+			success: function(data){
+				var names = data
+				$('allTweets').text(data);
+			}
+		});
+	}
+});
