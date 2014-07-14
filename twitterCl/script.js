44fe7b87b978c35c1a0baf640a86d37d7d3e3882
unknown <bernice.zerafa@MTDKP113.ix.com>
2014-07-14 09:46:49 +0200

    pretty date format. included jquery dateFormat plugin.

--- twitterCl/script.js
+++ twitterCl/script.js
@@ -66,7 +66,7 @@ function documentReadyList() {
 	if (hashtag != "") {
 		getTweetsByHashtags(hashtag);
 	}
-	if (username != "") {
+	else if (username != "") {
 		getTweetsByUser(username);
 	}
 }
@@ -126,47 +126,8 @@ function parseTags(content){
 }
 
 function getDateFormatForMs(timeMs){
-	var time = new Date(timeMs);
 	
-	// 11th July 2014
-	var curr_date = time.getDate();
-	var m_names = new Array("January", "February", "March", 
-	"April", "May", "June", "July", "August", "September", 
-	"October", "November", "December");
-	var sup = "";
-	if (curr_date == 1 || curr_date == 21 || curr_date ==31) {
-		sup = "st";
-	} else if (curr_date == 2 || curr_date == 22) {
-		sup = "nd";
-	} else if (curr_date == 3 || curr_date == 23) {
-		sup = "rd";
-	} else {
-		sup = "th";
-	}
-	var curr_month = time.getMonth();
-	var curr_year = time.getFullYear();
-
-	// H:mm AM
-	var a_p = "";
-	var curr_hour = time.getHours();
-	if (curr_hour < 12) {
-		a_p = "AM";
-	} else {
-		a_p = "PM";
-	}
-	if (curr_hour == 0) {
-		curr_hour = 12;
-	}
-	if (curr_hour > 12) {
-		curr_hour = curr_hour - 12;
-	}
-	var curr_min = time.getMinutes();
-	curr_min = curr_min + "";
-	if (curr_min.length == 1) {
-		curr_min = "0" + curr_min;
-	}
-	
-	return curr_date + "<SUP>" + sup + "</SUP> " + m_names[curr_month] + " " + curr_year + " " + curr_hour + ":" + curr_min + " " + a_p;
+	return $.format.prettyDate(timeMs);
 }
 
 function getStylingForTweet(tweet){
