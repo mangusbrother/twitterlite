175ab3649aac8c34e28139d8ef53b7500053edd6
Administrator <admin@local.host>
2014-07-14 10:45:17 +0200

    made a form around the post area. added a catch for undefined tiem

--- twitterCl/script.js
+++ twitterCl/script.js
@@ -63,10 +63,11 @@ function documentReadyList() {
 	var hashtag = getURLParameter("hashtag");
 	var username = getURLParameter("user");
 	
-	if (hashtag != "" && hashtag != undefined) {
+	if (hashtag) {
 		getTweetsByHashtags(hashtag);
 	}
-	else if (username != "" && username != undefined) {
+	
+	else if (username) {
 		getTweetsByUser(username);
 	}
 }
@@ -99,6 +100,7 @@ function getURLParameter(sParam)
         }
     }
 }
+
 function getTweetsByUser(username) {
 	
 	var url = "http://localhost:8080/twitterlite/messages/mention/" + username;
@@ -127,7 +129,9 @@ function parseTags(content){
 
 function getDateFormatForMs(timeMs){
 	
-	return $.format.prettyDate(timeMs);
+	var date = $.format.prettyDate(timeMs);
+	if(date != undefined) return date;
+	return "This post has traveled back in time";
 }
 
 function getStylingForTweet(tweet){
