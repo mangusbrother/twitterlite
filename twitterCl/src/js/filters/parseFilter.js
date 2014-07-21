63dde10ca83312b717a5bbc3582f97d59e4ed300
Kurt Cutajar <kurt.cutajar@ixaris.com>
2014-07-21 09:04:36 +0200

    Added comments to file.

--- twitterCl/src/js/filters/parseFilter.js
+++ twitterCl/src/js/filters/parseFilter.js
@@ -3,6 +3,11 @@
 angular.module('twitterlite.parseFilter', [])
 	.filter('parseTags', function() {
 		return function(content, type) {
+
+			/*
+			/ Define a covertor object which maps a type to the corresponding 
+			/ REGEX expression and replacement pattern.
+			*/
 			var convertors = {
 				hashtag : {
 					regex : /#([a-zA-Z0-9]+)/g,
@@ -18,14 +23,23 @@ angular.module('twitterlite.parseFilter', [])
 				}
 			};
 
-			var checkType = Object.keys(convertors).some(function(value) {
+			// If the given type does no match to any of the defined types, default to 'other'.
+			var isValidType = Object.keys(convertors).some(function(value) {
 				return value == type;
 			});
 
-			type = checkType ? type : 'other';
+			if (!isValidType) {
+				type = 'other';
+			}
+
+			// If the type is undefined, also default to 'other'.
 			var convertor = convertors[type || 'other'];
 
-			if (content) return content.replace(convertor.regex, convertor.pattern);
-			else return '';
+			/*
+			/ Carry out the conversion.
+			/ If the given content is undefined, default to the empty string.
+			*/
+
+			return content ? content.replace(convertor.regex, convertor.pattern) : '';
 		};
 });
